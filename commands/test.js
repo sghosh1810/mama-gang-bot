713c8440ca1dadc35dcb5f40e53572afc77e79dd
sghosh1810 <sghosh1810@gmail.com>
2021-08-17 00:08:51 +0530

    Test

--- commands/test.js
+++ commands/test.js
@@ -1,17 +1,17 @@
-const {MessageAttachment} = require('discord.js')
-const client = require("tnai")
-const tnai = new client();
+// const {MessageAttachment} = require('discord.js')
+// const client = require("tnai")
+// const tnai = new client();
 
-module.exports = {
-	name: 'test',
-	description: 'Test libs!',
-	async execute(message) {
-		await tnai.real.pussy().then(res => {
-            //console.log(res);
-            message.channel.send(new MessageAttachment(res))
-        }).catch(error => {
-			message.channel.send("An error occurred while fulfilling this request. Ask Duxorhell to check Sentry logs UwU");
-            console.log(error);
-        });
-	}
-};
+// module.exports = {
+// 	name: 'test',
+// 	description: 'Test libs!',
+// 	async execute(message) {
+// 		await tnai.real.pussy().then(res => {
+//             //console.log(res);
+//             message.channel.send(new MessageAttachment(res))
+//         }).catch(error => {
+// 			message.channel.send("An error occurred while fulfilling this request. Ask Duxorhell to check Sentry logs UwU");
+//             console.log(error);
+//         });
+// 	}
+// };
